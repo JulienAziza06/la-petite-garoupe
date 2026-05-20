1bc5b9b9935a7dab7e757290356ae226fc5e2d5c
weelsapp <julien.aziza@gmail.com>
2026-05-20 12:25:59 +0200

    Add Garoupe view image to place section

--- src/site/media.js
+++ src/site/media.js
@@ -3,7 +3,7 @@ import ambianceDrinks from './assets/images/home/ambiance-drinks.webp';
 import access from './assets/images/home/access.webp';
 import ambianceInterior from './assets/images/home/ambiance-interior.webp';
 import ambiancePizza from './assets/images/home/ambiance-pizza.webp';
-import placeImage from './assets/images/home/ambiance-top-left.webp';
+import placeImage from './assets/images/home/place-garoupe-view.webp';
 import cuisineBottomLeft from './assets/images/home/cuisine-bottom-left.webp';
 import cuisineBottomRight from './assets/images/home/cuisine-bottom-right.webp';
 import cuisineTopLeft from './assets/images/home/cuisine-top-left.webp';
